160caa414aeba6bcce31053958cc096c2edcd5d2
Alexander <alexandersilveraf@gmail.com>
2025-03-27 07:28:44 +0100

    commit tres

--- src/desafio_06/Premios.java
+++ src/desafio_06/Premios.java
@@ -7,14 +7,33 @@ package desafio_06;
  */
 public enum Premios {
 	
+	/** The pleno. */
 	PLENO,
+	
+	/** The seis sin. */
 	SEIS_SIN,
+	
+	/** The cinco con. */
 	CINCO_CON,
+	
+	/** The cinco sin. */
 	CINCO_SIN,
+	
+	/** The cuatro con. */
 	CUATRO_CON,
+	
+	/** The cuatro sin. */
 	CUATRO_SIN,
+	
+	/** The tres con. */
 	TRES_CON,
+	
+	/** The tres sin. */
 	TRES_SIN,
+	
+	/** The reintegro. */
 	REINTEGRO,
+	
+	/** The nada. */
 	NADA;
 }
