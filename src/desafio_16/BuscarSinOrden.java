8feb553b46827318dc49f32241b8d1eb90d6039c
Alexander <alexandersilveraf@gmail.com>
2025-04-10 09:23:35 +0200

    commit nueve

--- src/desafio_16/BuscarSinOrden.java
+++ src/desafio_16/BuscarSinOrden.java
@@ -1,8 +1,17 @@
 package desafio_16;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class BuscarSinOrden.
+ */
 public class BuscarSinOrden {
 	
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		int[] lista = {9, 3, 2, 7, 8};
 		System.out.println(buscar(lista, 7));
@@ -11,6 +20,13 @@ public class BuscarSinOrden {
 		System.out.println(buscarWhile(lista, 1));
 	}
 	
+	/**
+	 * Buscar.
+	 *
+	 * @param lista the lista
+	 * @param valor the valor
+	 * @return true, if successful
+	 */
 	public static boolean buscar(int[] lista, int valor) {
 		for (int n : lista) {
 			if (n == valor) {
@@ -20,6 +36,13 @@ public class BuscarSinOrden {
 		return false;
 	}
 	
+	/**
+	 * Buscar while.
+	 *
+	 * @param lista the lista
+	 * @param valor the valor
+	 * @return true, if successful
+	 */
 	public static boolean buscarWhile(int[] lista, int valor) {
 		boolean encontrado = false;
 		int i = 0;
--- src/desafio_17/BuscarConOrden.java
+++ src/desafio_17/BuscarConOrden.java
@@ -2,9 +2,18 @@ package desafio_17;
 
 import java.util.Arrays;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class BuscarConOrden.
+ */
 public class BuscarConOrden {
 
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		int[] lista = {9, 3, 2, 7, 8};
 		Arrays.sort(lista);
@@ -12,10 +21,26 @@ public class BuscarConOrden {
 		System.out.println(busquedaBinaria(lista, 1));
 	}
 	
+	/**
+	 * Busqueda binaria.
+	 *
+	 * @param lista the lista
+	 * @param elem the elem
+	 * @return true, if successful
+	 */
 	public static boolean busquedaBinaria(int[] lista, int elem) {
 		return busquedaBinariaAux(lista, elem, 0, lista.length-1);
 	}
 	
+	/**
+	 * Busqueda binaria aux.
+	 *
+	 * @param lista the lista
+	 * @param elem the elem
+	 * @param ini the ini
+	 * @param fin the fin
+	 * @return true, if successful
+	 */
 	private static boolean busquedaBinariaAux(
 			int[] lista, int elem, int ini, int fin) {
 		int centro = Math.floorDiv(fin - ini, 2) + ini;
